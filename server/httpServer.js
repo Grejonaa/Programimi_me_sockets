132da10b746701988e7be028ff42b2931b1f00f3
Jetaa <jeta.podrimcaku@student.uni-pr.edu>
2026-04-12 16:54:15 +0200

    Implement HTTP server

---
+++ server/httpServer.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const { clients, messages } = require('./tcpServer');
+const PORT = 8080;
+const server = http.createServer((req, res) => {
+  if(req.url === '/stats' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type' : 'aplication/json' });
+
+  const monitorData = {
+    status: "Online",
+    active_connections: clients.lengths,
+    total_messages: messages.length,
+    connected_ips: clients.map(c => c.remoteAddress),
+    last_messages: messages.slice(-5)
+  };
+    res.end(JSON.stringify(monitorData, null, 2));
+  }else{
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end("Per monitorin vizitoni / stats");
+  }
+});
+server.listen(Port, () => {
+  console.log('[HTTP] Monitori po punon ne http:/localhost:${PORT}/stats');
+});
