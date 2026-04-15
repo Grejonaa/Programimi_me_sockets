8983740655fc163ba5d0f94996de622247f9c881
hanahiseni <hana.hiseni@student.uni-pr.edu>
2026-04-15 15:58:57 +0200

    Pjesa e HTTP serverit

--- server/httpServer.js
+++ server/httpServer.js
@@ -1,23 +1,19 @@
-const http = require('http');
+const express = require('express');
 const { clients, messages } = require('./tcpServer');
-const PORT = 8080;
-const server = http.createServer((req, res) => {
-  if(req.url === '/stats' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type' : 'application/json' });
 
-  const monitorData = {
-    status: "Online",
-    active_connections: clients.length,
-    total_messages: messages.length,
-    connected_ips: clients.map(c => c.remoteAddress),
-    last_messages: messages.slice(-5)
-  };
-    res.end(JSON.stringify(monitorData, null, 2));
-  }else{
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end("Per monitorin vizitoni / stats");
-  }
+const app = express();
+
+app.get('/stats', (req, res) => {
+res.json({
+activeClients: clients.length,
+lastMessages: messages.slice(-10)
 });
-server.listen(PORT, () => {
-  console.log('[HTTP] Monitori po punon ne http:/localhost:'${PORT}/stats');
+});
+
+app.get('/health',(req,res)=>{
+  res.send("OK");
+});
+
+app.listen(8080,()=>{
+  console.log("HTTP server running on 8080");
 });
